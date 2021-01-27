5660819d7d4186e9483263c996c47d10614b4641
Кривошеев Дмитрий Владимирович <dvkrivosheev@sberbank.ru>
2021-01-27 21:58:21 +0300

    distance

--- src/index.js
+++ src/index.js
@@ -1,213 +1,177 @@
 const micro = require('micro');
 const R = require('ramda');
-const replies = require('./replies');
-const data = require('./data');
 
+// достопримечательности с координатами
+const sights = {
+  1: {
+    name: 'Фонтан Поцелуй',
+    location: {
+      lat: 59.915029,
+      lon: 30.511382, 
+    },
+    isVisited: false,
+  }
+}
 
 const server = micro(async (req, res) => {
-  const { version, request, session, state } = await micro.json(req);
-  const sessionState = state.session || false;
-  const userState = state.user || false;
+  const { session, request, state, version } = await micro.json(req);
+  const { location } = session;
+  const {session: { context }} = state;
+  const { type, command } = request;
+
+  function makeRequest({ text = '', directives = {}, endSession = false,  userStateUpdate = {}, context = '' , buttons = []}) {
+    const response = {
+      'response': {
+        'text': text,
+        'buttons': buttons,
+        'end_session': endSession,
+        'directives': directives,
+      },
+      'session_state': { 
+        context: context 
+      }, 
+      'user_state_update': userStateUpdate,
+      'version': version,
+    };
+    console.log('response:', JSON.stringify(response));
+    return response;
+  }
+
+
+  function askGeo() {
+    const directives = { "request_geolocation": {} };
+    const text = 'нужен доступ к геолокации';
+    return makeRequest({text, directives})
+  }
+
+  function sayGeo(location) {
+    const { lat, lon, accuracy } = location;
+    const context = 'sayGeo';
+    const buttons = [ { title: 'Сохранить', hide: true }, { title: 'Далее', hide: true } ];
+    const text = `Ваши координаты: ${lat} северной долготы, ${lon} южной широты. Погрешность ${accuracy}`;
+    return makeRequest({ text, context, buttons });
+  }
+
+  function saveGeo(location) {
+    const id = `f${(~~(Math.random()*1e8)).toString(16)}`;
+    const userStateUpdate = {location, ...{id: id}};
+    const { lat, lon, accuracy } = location;
+    const context = 'sayGeo';
+    const buttons = [ { title: 'Сохранить', hide: true }, { title: 'Далее', hide: true } ];
+    const text = `Ваши координаты: ${lat} северной долготы, ${lon} южной широты. Погрешность ${accuracy}`;
+    return makeRequest({ text, context, buttons, userStateUpdate });
+  }
+
+  function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
   
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
 
-  // варианты
-  /*
-  1. Новый пользователь, первый запуск: 
-    sessionState есть, но пустой
-    userState.gps || false = false
-    userTarget = false
-  2. Пользователь, у которого закончился срок предоставления геолокации
-    userState.gps.endTime < Date.Now()
+  function sayDistance() {
+    const { lat, lon, accuracy } = location;
+    const { lat: lat1, lon: lon1} = sights[1].location;
+    const distance = Math.trunc(getDistanceFromLatLonInKm(lat, lon, lat1, lon1) * 1000);
+    const context = 'sayDistance';
+    const buttons = [ { title: 'Далее', hide: true } ];
+    const text = `Расстояние до объекта ${distance} ± ${Math.trunc(accuracy)} метров`;
+    return makeRequest({ text, context, buttons });
+  }
 
+  function handler() {
+    console.log('request:', JSON.stringify(request));
+    console.log('state:', JSON.stringify(state));
 
+    
 
-  */
+    if(location) {
+      if(/сохранить/i.test(command) && context === 'sayGeo') {
+        return saveGeo(location);
+      } else {
+        return sayDistance();
+      }
+    } else {
+      return askGeo();
+    }
+  }
 
-  
+  return handler();
 
-  return checkAnswer(state, request.command, version, session, request)
 });
 
 
- 
-function isGeoAllowed(time) {
-  const currentTime = Date.now();
-  return currentTime < time;
-}
-
-function checkAnswer(state, command, version, session, intent) {
-  // const intents = Object.keys(intent);
-
-  
-  const context = state.session && state.session.context; /*
-  показывает на какую тему был задан вопрос в предыдущем request-e
-   location access - запросили доступ к геолокации
-   last game - у пользователя есть прогрес в игре и мы справшиваем хочет ли он продолжить старую игру или начать новую
-   */
-  console.log(context);
-  
-  // default response
-  let response = {
-    "response": {
-      "text": "",
-      "end_session": false
-    },
-    "version": version
-  }
-
-  if (session.new) {
-    response.response["text"] += 'Приветствую! Механика навыка такая-то.\n';
-
-  }
-
-  const userTarget = state.user && state.user.target;
-  if (!userTarget) {
-    return;
-    // новый пользователь. добавить данные в state.user
-    response["user_state_update"] = {
-      target: 1000, 
-      data: {
-        1: {
-          nameObj: 'Тысячелетие России',
-          location: { x: 20, y: 20 },
-          reference: 'Купол железный',
-          isVisited: false
-        },
-      }
-    };
-  }
-
-
-  const gpsAccess = state.user.gps && state.user.gps.endTime;
-  console.log(response.session_state && response.session_state.context);
-  
-  if (!session.location && !(response.session_state && response.session_state.context)) {
-    // разрешение на доступ гео-локации просрочено или отсутствует
-    // запросить разрешение
-    response.response["text"] += 'Для работы навыка нужен доступ к гео-локации';
-    // response.response["buttons"] = [{ title: 'Да', hide: true }, { title: 'Нет', hide: true }];
-    response.response["directives"] = {"request_geolocation": {}};
-    response['session_state'] = {"context": "location access"}; // сохраняем в сессии признак, чтобы потом понять когда не него ответят
-  };
-  
-
-  if(userTarget!==1000 && !(response.session_state && response.session_state.context)) {
-    response.response["text"] += 'У вас есть загаданный объект. Прододжить или начать новую игру?';
-    response.response["buttons"] = [{ title: 'Продолжить', hide: true }, { title: 'Новая игра', hide: true }];
-    response['session_state'] = {"context": "last game"}; // сохраняем в сессии признак, чтобы потом понять когда не него ответят
-  }
-
-
-  // ДА
-  if (/да/i.test(command)) {
-    // Ответ на запрос по поводу представления доступа к гео-локации
-    if(context === "location access") {
-      // рассчитать время окончания доступа
-      const newEndTime = Date.now() + 1*60*60*1000 // даём доступ на час. час переводим в миллисекунды
-      // добавить в state.user.gps
-      response["user_state_update"] = { gps: { endTime: newEndTime } };
-      response['session_state'] = {};
-    }
-  }
-
-  // НЕТ
-  if (/нет/i.test(command)) {
-    // Ответ на запрос по поводу представления доступа к гео-локации
-    if(context === "location access") {
-      // выход из навыка
-      response.response["text"] = 'До свидания';
-      response.response["end_session"] = true;
-    }
-  }
-
-  // Продожить
-  if (/продолжить/i.test(command)) {
-    // Ответ на запрос по поводу продолжения предущей игры
-    if(context === "last game") {
-      // Перейти к игре
-      response['session_state'] = {};
-    }
-  }
-
-  // Продожить
-  if (/Новая игра/i.test(command)) {
-    // Ответ на запрос по поводу продолжения предущей игры
-    if(context === "last game") {
-      // Перезапиcать target
-      response["user_state_update"] = { target: 1 };
-
-      // Перейти к игре
-      response['session_state'] = {};
-
-    }
-  }
-
-  // очистить стейт
-  if (command === 'clear all') {
-    response["user_state_update"] = { 
-      target: 1,
-      data: null,
-      gps: null,
-    };
-
-  }
-
-
-
-  
-
-
- 
-
-
-
-  // // добавить в стейт state.user data еслиесли его нет 
-
-  // if (/сдаюсь/i.test(command)) {
-  //   const { number1, number2 } = sessionState.question;
-  //   const answer = number1 + number2;
-  //   question = generateQuestion(sessionState);
-  //   return replies.capitulate(answer, question);
-  // }
-
-  // if (/давай до свидания/i.test(command)) {
-  //   return replies.farewell();
-  // }
-
-  // let question = sessionState.question;
-  // if (!question) {
-  //   question = generateQuestion(sessionState);
-  //   return replies.firstQuestion(question);
-  // }
-
-  // if (isCorrectAnswer(question, command)) {
-  //   question = generateQuestion(sessionState);
-  //   return replies.correctAnswer(question);
-  // }
-
-  // return replies.incorrectAnswer(question);
-
-  const loc = JSON.stringify(session.location);
-  if (loc) response.response["text"] = `\n Ваши координаты ${loc}`;
-  console.log('location', loc);
-
-  return response
-}
-
-function isCorrectAnswer(question, command) {
-  const matches = command.match(/[0-9]+/);
-  const correctAnswer =  question.number1 + question.number2;
-  return matches && Number(matches) === correctAnswer;
-}
-
-function generateQuestion(sessionState) {
-  const question = {
-    number1: Math.ceil(Math.random() * 20),
-    number2: Math.ceil(Math.random() * 20),
-  };
-  sessionState.question = question;
-  return question;
-}
-
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
+
+/*
+{
+  "meta": {
+    "locale": "ru-RU",
+    "timezone": "UTC",
+    "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
+    "interfaces": {
+      "screen": {},
+      "payments": {},
+      "account_linking": {},
+      "geolocation_sharing": {}
+    }
+  },
+  "session": {
+    "message_id": 1,
+    "session_id": "ee67f0c1-f01c-4150-8c61-f109ff5183d9",
+    "skill_id": "5fb9aa09-f841-499d-8a83-f469b5448553",
+    "user": {
+      "user_id": "FFCBB8172A1D1C4BD4E7B37EE3C20CC895D4716DF808C8473D8A8B95E9EAE456"
+    },
+    "application": {
+      "application_id": "9CCE201EB1888F74D54490F39097517E030FAA3209C69D396F2E5B6A0708E10A"
+    },
+    "user_id": "9CCE201EB1888F74D54490F39097517E030FAA3209C69D396F2E5B6A0708E10A",
+    "new": false
+  },
+  "request": {
+    "command": "да",
+    "original_utterance": "Да",
+    "nlu": {
+      "tokens": [
+        "да"
+      ],
+      "entities": [],
+      "intents": {
+        "YANDEX.CONFIRM": {
+          "slots": {}
+        },
+        "agree": {
+          "slots": {}
+        }
+      }
+    },
+    "markup": {
+      "dangerous_context": false
+    },
+    "type": "SimpleUtterance"
+  },
+  "state": {
+    "session": {
+      "context": "location access"
+    },
+    "user": {
+      "target": 1
+    },
+    "application": {}
+  },
+  "version": "1.0"
+}
+*/
